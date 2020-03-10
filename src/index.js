f451795c9063e91efe821b4758cfb288bdac9975
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-03-10 23:57:48 +0300

    feat: complete task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const keys = bracketsConfig.map(item => item[0]);
+  const vals = bracketsConfig.map(item => item[1]);
+
+  let mass = str.split("");
+  let stack = [];
+
+  let sum = 0;
+  for (let i = 0; i < mass.length; i++) {
+      if (keys.indexOf(mass[i]) + 1 && vals.indexOf(mass[i]) + 1) {
+          mass.splice(i, 1, "");
+          sum++;
+      }
+  }
+  if (sum % 2 !== 0) return false;
+
+  for (let i = 0; i < mass.length; i++) {
+      const l = mass[i];
+
+      if (keys.indexOf(l) + 1) {
+          stack.push(l);
+      } else {
+          if (vals.indexOf(l) + 1) {
+              let v = vals.indexOf(l);
+              let k = keys.indexOf(stack.pop());
+              if (k !== v) return false;
+          }
+      }
+  }
+
+  if (stack.length) {
+      return false;
+  }
+
+  return true;
+
+};
